be8ba7b4e92ad068b912c23f33339f04ed43275f
Hycarus <mirkomarasco08@gmail.com>
2024-01-11 17:26:39 +0100

    added characters model

---
+++ database/seeders/CharacterSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Character;
+
+class CharacterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $json = file_get_contents(__DIR__ . '/data/characters.json');
+        $content =  json_decode($json, true);
+        foreach ($content as $characterData) {
+            $item = new Character();
+            $item->name = $characterData['name'];
+            $item->description = $characterData['description'];
+            $item->attack = $characterData['attack'];
+            $item->defence = $characterData['defence'];
+            $item->speed = $characterData['speed'];
+            $item->life = $characterData['life'];
+            $item->save();
+        }
+    }
+}
