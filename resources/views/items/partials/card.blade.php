04b06834cb999e958f25ef5583a1f67e1e8d58c6
iaio998 <depalatisiacopo@gmail.com>
2024-01-11 17:42:51 +0100

    Fixed space in card blade

--- resources/views/items/partials/card.blade.php
+++ resources/views/items/partials/card.blade.php
@@ -1,5 +1,4 @@
 <div class="col-2">
-
     <div class="text-center border p-1">
         <h2 class="fs-6 ">Name: {{$item->name}}</h2>
         <p class="fst-italic m-0">Slug: {{$item->slug}}</p>
@@ -8,5 +7,4 @@
         <p class="fst-italic m-0">Cost: {{$item->cost}}</p>
         <a href="{{route('items.show', $item->id)}}" class="btn btn-primary">See detail</a>
     </div>
-
 </div>
