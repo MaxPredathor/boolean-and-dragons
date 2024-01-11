e5bb18b48a348ebdc6e201cb72f1d7fc214a019b
iaio998 <depalatisiacopo@gmail.com>
2024-01-11 16:57:32 +0100

    Added seed in db

--- database/migrations/2024_01_11_155503_create_items_table.php
+++ database/migrations/2024_01_11_155503_create_items_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('name', 200);
-            $table->text('description')->nullable();
             $table->string('slug', 200);
             $table->string('category', 100);
             $table->string('type', 100);
--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Item;
 
 class ItemSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class ItemSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $json = file_get_contents(__DIR__ . '/data/items.json');
+        $items = json_decode($json, true);
+        // dd($items);
+        foreach ($items as $item) {
+            $newItem = new Item();
+            $newItem->name = $item['name'];
+            $newItem->slug = $item['slug'];
+            $newItem->category = $item['category'];
+            $newItem->type = $item['type'];
+            $newItem->weight = $item['weight'];
+            $newItem->cost = $item['cost'];
+            $newItem->save();
+        }
     }
 }
