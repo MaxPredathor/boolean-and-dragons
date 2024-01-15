ea0b11b3227b3991a94d37f4d6febf9e60eb76a4
MaxPredathor <max.gilli98@gmail.com>
2024-01-15 18:28:15 +0100

    styled show

--- resources/views/admin/items/show.blade.php
+++ resources/views/admin/items/show.blade.php
@@ -5,10 +5,10 @@
         <a href="{{ route('admin.items.index') }}" class="btn btn-danger">Return</a>
         <div class="row justify-content-center">
             <div class="col">
-                <div class="text-center border p-1">
+                <div class="text-center border p-1 text-white rounded" style="background-color: #272822">
                     <h2 class="fs-6 ">Name: {{ $item->name }}</h2>
                     <div>
-                        <img class="w-50" src="{{ Vite::asset('public/images/items_images/' . $item->name . '.png') }}"
+                        <img class="w-25" src="{{ Vite::asset('public/images/items_images/' . $item->name . '.png') }}"
                             alt="">
                     </div>
                     <p class="fst-italic m-0">Slug: {{ $item->slug }}</p>
@@ -16,8 +16,8 @@
                     <p class="fst-italic m-0">Weight: {{ $item->weight }}</p>
                     <p class="fst-italic m-0">Cost: {{ $item->cost }}</p>
                     <div class="d-flex justify-content-center gap-2">
-                        <a href="{{ route('admin.items.edit', $item->id) }}" class="btn btn-primary">Edit</a>
-                        <form action="{{ route('admin.items.destroy', $item->id) }}" method="POST">
+                        <a href="{{ route('admin.items.edit', $item->id) }}" class="btn btn-primary mb-4 mt-2">Edit</a>
+                        <form class="mb-4 mt-2" action="{{ route('admin.items.destroy', $item->id) }}" method="POST">
                             @csrf
                             @method('DELETE')
                             <button id="confirm-delete" class="cancel-button btn btn-danger"
