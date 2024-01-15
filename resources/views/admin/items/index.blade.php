ebd16bbe1091a7138e38e10ef3519e445e138320
MaxPredathor <max.gilli98@gmail.com>
2024-01-15 18:30:25 +0100

    fixed margin

--- resources/views/admin/items/index.blade.php
+++ resources/views/admin/items/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container py-2">
         <h1 class="d-inline-block">Items Index</h1>
-        <a href="{{ route('admin.items.create') }}" class="btn btn-success">Add new</a>
+        <a href="{{ route('admin.items.create') }}" class="btn btn-success ms-2 mb-2">Add new</a>
         @if (session()->has('success'))
             <div class="alert alert-success">
                 {{ session('success') }}
