ea2e9a782c325d1c5ef424175cee611103b98974
iaio998 <depalatisiacopo@gmail.com>
2024-01-25 09:31:56 +0100

    Fixed viewport in types pages

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -1,27 +1,26 @@
-@include('layouts.partials.header')
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>
-            All classes</h1>
-        <a href="{{ route('admin.types.create') }}" class="btn btn-success">Add a new class</a>
-        <div class="row">
-            @if (session()->has('message'))
-                <div class="alert alert-success my-5">{{ session('message') }}</div>
-            @endif
-            @foreach ($types as $type)
-                <div class="col-3 my-2">
-                    <div class="card h-100">
-                        <img src="{{ $type->image ? asset('storage/' . $type->image) : 'https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg' }}"
-                            style="height: 300px;" class="card-img-top w-100" alt="{{ $type->name }}">
-                        <div class="card-body d-flex justify-content-between">
-                            <h3 class="card-title">{{ $type->name }}</h3>
-                            <a href="{{ route('admin.types.show', $type->slug) }}" class="btn btn-danger">View More</a>
-                        </div>
-                    </div>
+<div class="container">
+    <h1>
+        All classes</h1>
+    <a href="{{ route('admin.types.create') }}" class="btn btn-success">Add a new class</a>
+    <div class="row">
+        @if (session()->has('message'))
+        <div class="alert alert-success my-5">{{ session('message') }}</div>
+        @endif
+        @foreach ($types as $type)
+        <div class="col-3 my-2">
+            <div class="card h-100">
+                <img src="{{ $type->image ? asset('storage/' . $type->image) : 'https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg' }}"
+                    style="height: 300px;" class="card-img-top w-100" alt="{{ $type->name }}">
+                <div class="card-body d-flex justify-content-between">
+                    <h3 class="card-title">{{ $type->name }}</h3>
+                    <a href="{{ route('admin.types.show', $type->slug) }}" class="btn btn-danger">View More</a>
                 </div>
-            @endforeach
+            </div>
         </div>
+        @endforeach
     </div>
+</div>
 @endsection
