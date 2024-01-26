225fc587d1759ef1b08893639a2175c873e74147
iaio998 <depalatisiacopo@gmail.com>
2024-01-26 12:32:48 +0100

    Finished baldes uploaded for types

--- resources/views/admin/types/create.blade.php
+++ resources/views/admin/types/create.blade.php
@@ -5,10 +5,10 @@
 @section('content')
 
 
-    <div class="">
-        <div class="container">
-            <h2>Create a new Character Type</h2>
-            {{-- @if ($errors->any())
+<div class="">
+    <div class="container">
+        <h2>Create a new Character Type</h2>
+        {{-- @if ($errors->any())
         <div class="alert alert-danger my-2">
             <ul>
                 @foreach ($errors->all() as $error)
@@ -17,72 +17,75 @@
             </ul>
         </div>
         @endif --}}
-            <div class="row">
-                <form action="{{ route('admin.types.store') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <input required class="form-control my-1 @error('name') is-invalid @enderror" type="text" id="name"
-                        name="name" placeholder="Name">
-                    @error('name')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                    <textarea required rows="8" class="form-control my-1 @error('desc') is-invalid @enderror" type="text"
-                        id="desc" name="desc" placeholder="Description"></textarea>
-                    @error('desc')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                    <label for="image">Image</label>
-                    <div class="my-3" style="width: 200px;">
-                        <img class="w-100" id="uploadPreview"
-                            src="https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg"
-                            alt="Placeholder">
-                    </div>
-                    <input type="file" accept="image/*" name="image" id="image" class="form-control my-1">
-                    @error('image')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-
-
-                    <label for="base_sprite">Base Sprite</label>
-                    <div class="my-3" style="width: 200px;">
-                        <img class="w-100" id="uploadPreview"
-                            src="https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg"
-                            alt="Placeholder">
-                    </div>
-                    <input type="file" accept="image/*" name="base_sprite" id="base_sprite" class="form-control my-1">
-                    @error('base_sprite')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-
-
-                    <label for="ascended_sprite">Ascended Sprite</label>
-                    <div class="my-3" style="width: 200px;">
-                        <img class="w-100" id="uploadPreview"
-                            src="https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg"
-                            alt="Placeholder">
-                    </div>
-                    <input type="file" accept="image/*" name="ascended_sprite" id="ascended_sprite"
-                        class="form-control my-1">
-                    @error('ascended_sprite')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-
-
-                    <button class="btn
-                            btn-primary mt-2" type="submit">Add</button>
-                    <input type="reset" class="btn btn-danger mt-2" value="Reset">
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <div class="final-one">
-        <div class="container">
-            <div class="row">
-                <div>
-
+        <div class="row">
+            <form action="{{ route('admin.types.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <input required class="form-control my-1 @error('name') is-invalid @enderror" type="text" id="name"
+                    name="name" placeholder="Name">
+                @error('name')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <input required class="form-control my-1 @error('boosted_stat') is-invalid @enderror" type="text"
+                    id="boosted_stat" boosted_stat="boosted_stat" placeholder="boosted_stat">
+                @error('boosted_stat')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <textarea required rows="8" class="form-control my-1 @error('desc') is-invalid @enderror" type="text"
+                    id="desc" name="desc" placeholder="Description"></textarea>
+                @error('desc')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <label for="image">Image</label>
+                <div class="my-3" style="width: 200px;">
+                    <img class="w-100" id="uploadPreview"
+                        src="https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg"
+                        alt="Placeholder">
                 </div>
+                <input type="file" accept="image/*" name="image" id="image" class="form-control my-1">
+                @error('image')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <label for="base_sprite">Base Sprite</label>
+                <div class="my-3" style="width: 200px;">
+                    <img class="w-100" id="uploadPreview"
+                        src="https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg"
+                        alt="Placeholder">
+                </div>
+                <input type="file" accept="image/*" name="base_sprite" id="base_sprite" class="form-control my-1">
+                @error('base_sprite')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+
+
+                <label for="ascended_sprite">Ascended Sprite</label>
+                <div class="my-3" style="width: 200px;">
+                    <img class="w-100" id="uploadPreview"
+                        src="https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg"
+                        alt="Placeholder">
+                </div>
+                <input type="file" accept="image/*" name="ascended_sprite" id="ascended_sprite"
+                    class="form-control my-1">
+                @error('ascended_sprite')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+
+
+                <button class="btn
+                            btn-primary mt-2" type="submit">Add</button>
+                <input type="reset" class="btn btn-danger mt-2" value="Reset">
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="final-one">
+    <div class="container">
+        <div class="row">
+            <div>
+
             </div>
         </div>
     </div>
+</div>
 
 @endsection
